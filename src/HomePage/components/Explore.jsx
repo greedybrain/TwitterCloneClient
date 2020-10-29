d9f63bf424593a1ddb728a8a8d16edca6dfa2ed4
greedybrain <greedybrain18@gmail.com>
2020-10-29 17:54:51 -0400

    created explore component

---
+++ src/HomePage/components/Explore.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../../App/components/Header';
+
+class Explore extends Component {
+        render() {
+                return (
+                        <div className="explorer_wrapper">
+                                <Header />
+                        </div>
+                );
+        }
+}
+
+export default Explore;
