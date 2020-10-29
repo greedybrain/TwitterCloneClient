ff57054b7d9527b55c85728033528d58238ff6dc
greedybrain <greedybrain18@gmail.com>
2020-10-29 17:53:42 -0400

    removed default app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./App/components/Header";
+import Navbar from "./App/components/Navbar";
+import Routes from "./Routes";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes />
+      <div className="create_tweet">
+              <i className="fas fa-feather-alt"></i>
+      </div>
+      <Navbar />
     </div>
   );
 }
