4ccc5c76dd9bd4e5281360d7047aba3bc39b2512
greedybrain <greedybrain18@gmail.com>
2020-10-29 20:50:03 -0400

    added post skeleton

--- src/HomePage/components/Tweet.jsx
+++ src/HomePage/components/Tweet.jsx
@@ -1,32 +1,57 @@
 import React from 'react'
+import './Tweet.css'
 
-const Tweet = () => {
+const Tweet = ({ user }) => {
         return (
                 <div className="tweet">
                         <div className="tweet_head">
 
                         </div>
                         <div className="tweet_body">
-                                <div className="tweeter">
-                                        {/* profile image  */}
+                                <div className="image_wrapper">
+                                        <div className="tweeter">
+                                                <img src={user.image} alt="pic"/>
+                                        </div>
                                 </div>
                                 <div className="tweet_content">
                                         <div className="tweet_content_head">
-                                                <div className="tweeter_name"></div>
-                                                <div className="tweet_time"></div>
-                                                <div className="tweet_settings"></div>
+                                                <div className="tweeter_name">
+                                                        <span>
+                                                                {user.name.length > 15 ? user.name.slice(0, 15)  : user.name} 
+                                                                <span className="at_name"> @{ user.username.length > 2 ? user.username.slice(0, 2) + '...' : user.username }</span>
+                                                        </span>
+                                                </div>
+                                                <div className="tweet_time">
+                                                        <span className="bullet">•</span>
+                                                        <span className="time"> 2h</span>
+                                                </div>
+                                                
                                         </div>
                                         <div className="content">
-
+                                                {user.content}
+                                                <div className="contentImage">
+                                                        <img src={user.contentImage} alt="pic"/>
+                                                </div>
+                                        </div>
+                                        <div className="tweet_footer">
+                                                <ul>
+                                                        <li className="comments item">
+                                                                <i class="far fa-comment"></i>
+                                                        </li>
+                                                        <li className="retweets item">
+                                                                <i class="fas fa-recycle"></i>
+                                                        </li>
+                                                        <li className="heart item">
+                                                                <i class="far fa-heart"></i>
+                                                        </li>
+                                                        <li className="other item">
+                                                                <i class="fas fa-arrow-up"></i>
+                                                        </li>
+                                                </ul>
                                         </div>
                                 </div>
-                                <div className="tweet_footer">
-                                        <ul>
-                                                <li className="comments item"></li>
-                                                <li className="retweets item"></li>
-                                                <li className="heart item"></li>
-                                                <li className="other item"></li>
-                                        </ul>
+                                <div className="tweet_settings">
+                                        <i class="fas fa-ellipsis-h"></i>
                                 </div>
                         </div>
                 </div>
