bed86b24b44952c6de946a25753d10115ad90c1f
greedybrain <greedybrain18@gmail.com>
2020-10-30 12:27:31 -0400

    added createtweet button

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import Header from "./App/components/Header";
 import Navbar from "./App/components/Navbar";
 import Routes from "./Routes";
 import './App.css'
+import CreateTweetButton from "./App/components/CreateTweetButton";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes />
-      <div className="create_tweet">
-              <i className="fas fa-feather-alt"></i>
-      </div>
+      <CreateTweetButton />
       <Navbar />
     </div>
   );
