b0a2f899d850312a89609c3ef3c3497fd42ae7a2
greedybrain <greedybrain18@gmail.com>
2020-10-30 12:27:45 -0400

    added route for compose tweet page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import ComposeTweet from './App/components/ComposeTweet'
 import Explore from './HomePage/components/Explore'
 import Home from './HomePage/containers/Home'
 
@@ -12,6 +13,11 @@ const Routes = () => {
                                         path='/explore'
                                         render={routerProps => <Explore {...routerProps} pageTitle="Explore" />}
                                 /> 
+                                <Route 
+                                        exact
+                                        path='/compose/tweet'
+                                        render={routerProps => <ComposeTweet {...routerProps} />}
+                                />
                                 <Route 
                                         exact
                                         path="/"
