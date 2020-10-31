822a16173b5babf6a71ae295bef3483aee403d94
greedybrain <greedybrain18@gmail.com>
2020-10-30 21:34:28 -0400

    added header explicitly to home component

--- src/HomePage/containers/Home.jsx
+++ src/HomePage/containers/Home.jsx
@@ -6,6 +6,22 @@ class Home extends Component {
         render() {
                 return (
                         <div className="home_wrapper">
+                                <header className='header'> 
+                                        <div className="image_and_label">
+                                                <div className="profile_image">
+                                                        {/* profile image  */}
+                                                        {/* <img src={naya_willis_image} alt="profile pic"/> */}
+                                                </div>
+                                                <div className="label">
+                                                        {/* label  */}
+                                                        <h2>Home</h2>
+                                                </div>
+                                        </div>
+                                        <div className="header_icon">
+                                                
+                                                <i className="far fa-star"></i> 
+                                        </div>
+                                </header>
                                 <Tweets />
                         </div>
                 );
