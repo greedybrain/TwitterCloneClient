53030976cbf880263af98fb7c0418ae6bfb51b7d
greedybrain <greedybrain18@gmail.com>
2020-10-30 21:32:29 -0400

    added styling

---
+++ src/App/components/CreateMessageButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CreateMessageButton.css'
+
+const CreateMessageButton = () => {
+        return (
+                <div className="create_message">
+                        <i class="far fa-envelope"></i>
+                </div>
+        )
+}
+
+export default CreateMessageButton
