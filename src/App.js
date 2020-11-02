165870e0905f668e08fdae2e40862d4343e3f3b1
greedybrain <greedybrain18@gmail.com>
2020-11-02 10:58:56 -0500

    ...

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Navbar from "./App/components/Navbar";
 import Routes from "./Routes";
 import './App.css'
 import CreateTweetButton from "./App/components/CreateTweetButton";
+import AccountInfo from "./App/components/AccountInfo";
+import { useState } from "react";
 
 function App() {
+  const [accountInfoDisplayed, setAccountInfoDisplayed] = useState(false)
   return (
     <div className="App">
       {/* <Header /> */}
-      <Routes />
+      {
+        accountInfoDisplayed && (
+          <AccountInfo accountInfoDisplayed={accountInfoDisplayed} setAccountInfoDisplayed={setAccountInfoDisplayed} />
+        )
+      }
+      <Routes setAccountInfoDisplayed={setAccountInfoDisplayed} />
       <CreateTweetButton />
       <Navbar />
     </div>
