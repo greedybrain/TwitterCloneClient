af9f12e88ba7a65dccf4e048f2cc77bfb191c525
greedybrain <greedybrain18@gmail.com>
2020-11-02 10:57:00 -0500

    refac'd header compnent (mino)

--- src/App/components/Header.jsx
+++ src/App/components/Header.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 // import naya_willis_image from '../../Images/naya_willis.jpg'
 import './Header.css'
+import ProfileImageOpensAccountInfo from './ProfileImageOpensAccountInfo'
 
 const Header = () => {
         return (
                 <header className='header'> 
                         <div className="image_and_label">
-                                <div className="profile_image">
-                                        {/* profile image  */}
-                                        {/* <img src={naya_willis_image} alt="profile pic"/> */}
-                                </div>
-                                <div className="label">
-                                        {/* label  */}
-                                        <h2>Home</h2>
-                                </div>
+                                <ProfileImageOpensAccountInfo />
                         </div>
                         <div className="header_icon">
-                                
                                 <i className="far fa-star"></i> 
                         </div>
                 </header>
